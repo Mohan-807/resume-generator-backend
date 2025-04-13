c274f1d51efc7cef933e30be532c1bc11da9c342
Mohan <mohan8105072435@gmail.com>
2025-04-13 19:58:31 +0530

    small change

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,7 +12,7 @@ import { TemplatesModule } from './templates/templates.module';
       port: Number(process.env.DB_PORT) || 3306,
       username: process.env.DB_USER || 'root',
       password: process.env.DB_PASSWORD || '',
-      database: process.env.DB_NAME || 'my_database',
+      database: process.env.DB_NAME || 'resume-builder',
       autoLoadEntities: true,
       synchronize: true,
     }),
